0b673605245a346cfa904414f89519626ae79f1e
firezihai <zihaidetiandi@sina.com>
2016-04-27 15:23:49 +0800

    修复不能轮播导航点击后无法自动播,showDefNav设置false之后,不能再生成导航,需用户自已编写,并设置navClass和navItem两个参数,优化fade动画

--- js/jquery.zSlider.js
+++ js/jquery.zSlider.js
@@ -21,14 +21,15 @@
 			animate: 'animate',
 			direction : 'horizontal',//滚动方向,vertical重直滚动,horizontal水平滚动
 			event:"mouseover",
-			fadeInTime:2000,
-			fadeOutTime:2000,
+			fadeInTime:1000,
+			fadeOutTime:1000,
 			showDefNav: true,		//是否使用默认导航
-			navClass: 'nav',
+			navClass: '.nav',
+			navItem:'span',
 			navHoverColor:'orange',	//导航激活之后的颜色
 			navColor:'#ffffff',				//导航默认颜色
 			showDefControl : false,			//是否显示上一张下一张的控制按钮
-			controlClass:'control',
+			controlClass:'.control',
 			duration: 3000,		//播放频率
 			speed : 500,		//滚动速度
 			auto : true						//是否自动播放
@@ -39,44 +40,45 @@
 	zSlider.prototype ={
 		init : function(){
 			var that = this;//储存slider对象
-			this.itemWrap = this.$element.find('.'+this.options.itemWrap);
-			this.item = this.$element.find('.'+this.options.item);
+			this.itemWrap = this.$element.find(this.options.itemWrap);
+			this.item = this.itemWrap.find(this.options.item);
 			this.number = this.item.length;
 			this.width = this.item.first().outerWidth();
 			this.height =  this.item.first().outerHeight();
-			console.log(this.height);
-			this.$element.css({'position':'relative'});
-			this.itemWrap.css({'position':'absolute',top:'0px',left:'0px'});
 			if(this.options.direction == 'horizontal' && this.options.animate == 'animate'){
+				this.itemWrap.css({'position':'absolute',top:'0px',left:'0px'});
 				this.itemWrap.css({'width':this.width*this.number+'px'});
 				this.item.css({'float':'left'});
+				if(this.options.fullScreen){
+					this.item.css({'width':$(window).width()});
+					$(window).resize(function(){
+						this.item.css({'width':$(window).width()});
+					});
+				}
 			}
 			/*设置默认的导航*/
 			if(this.options.showDefNav){
 				this.createNav();
 			}
 			if(this.options.showDefControl){
-				this.$element.append('<div class="'+this.options.controlClass+'"><a href="javascript:;" class="prevBtn"></a><a href="javascript:;"  class="nextBtn"></a></div>');
-				var $control = this.$element.find('.'+this.options.controlClass);
+				this.$element.append('<div class="'+this.options.controlClass.substr(1)+'"><a href="javascript:;" class="prev-btn"></a><a href="javascript:;"  class="next-btn"></a></div>');
+				var $control = this.$element.find(this.options.controlClass);
 				$control.find('a').css({'position':'absolute','width':'40px','height':'40px','cursor':'pointer','background':'orange','opacity':'0.4'});
 				var top = this.height*0.4;
 				$control.find('.prevBtn').css({'left':'10px','z-index':'4','top':top+'px'});
 				$control.find('.nextBtn').css({'right':'10px','z-index':'4','top':top+'px'});
 			}
 			if(this.options.animate == 'fade'){//如果图片是淡入淡出动画，强制设置第一个图片为显示
-				this.item.css({'display':'none'});
-				this.item.eq(0).css({'display':'block'});
+				this.item.css({'position': 'absolute','display':'none','opacity':0,'z-index':0,});
+				this.item.eq(0).css({'position': 'absolute','display':'block','opacity':1,'z-index':2});
 			}
-			/*获取导航元素*/
-			if(this.options.showDefNav){
-				 this.$nav = this.$element.find('.nav span');
-			}else{
-				this.$nav =this.$element.find('.'+this.options.navClass).children();
-			}
+		
+			this.$nav =this.$element.find(this.options.navClass).find(this.options.navItem);
+			
 			this.item.mouseover(function(){
 					clearInterval(that.$element.timer);
 				}).mouseout(function(){
-					if(that.options) that.play();
+					if(that.options.auto) that.play();
 				});
 			
 			/*上一张下一张*/
@@ -89,13 +91,14 @@
 			if(this.options.auto)	this.play();
 		},
 		createNav:function(){
-				this.$element.append('<div class="'+this.options.navClass+'"></div>');
-				var $nav = this.$element.find('.'+this.options.navClass);
+				var navClass = this.options.navClass.substr(1);
+				this.$element.append('<div class="'+navClass+'"></div>');
+				var $nav = this.$element.find(this.options.navClass);
 				for(var i =1;i<=this.number;i++){
 					$nav.append('<span>●</span>')
 				}
-				$nav.css({'position':'absolute','z-index':3,'left':'50%','bottom':'20px','text-align':'center','font-size':'0','border-radius':'10px','background-color':' rgba(255,255,255,0.3)','filter': 'alpha(opacity:30)'});
-				$nav.find('span').css({'display':'inline-block','font-size':'14px','color':this.options.navColor,'text-decoration':'none','cursor':'pointer','margin':'2px'});
+				$nav.css({'position':'absolute','z-index':3,'left':'50%','bottom':'20px','text-align':'center','font-size':'0','border-radius':'10px'});
+				$nav.find('span').css({'display':'inline-block','font-size':'14px','color':this.options.navColor,'text-decoration':'none','cursor':'pointer','margin':'2px','font-family': 'sans-serif'});
 				$nav.find('span:first').addClass('on');
 				$nav.find('.on').css({'color':this.options.navHoverColor});
 				var nav_margin_left = $nav.width()*(-0.5);//获取导航margin-left的偏移量，必需先设置好span的大小之后在获取,否则获取的将是父素的宽度
@@ -104,7 +107,7 @@
 		/*上一张下一张*/
 		prev_and_next: function(type){
 			var that = this;//slider对象的this储存起来,以便在jquery的函数中使用
-			this.$element.find('.'+this.options.controlClass+' .'+type+'Btn').click(function(event){
+			this.$element.find(this.options.controlClass+' .'+type+'-btn').click(function(event){
 					if(type =='prev'){
 						that.index--;
 					}else if(type == 'next'){
@@ -112,6 +115,11 @@
 					}
 					clearInterval(that.$element.timer);
 					that.sliderNext();
+			}).mouseout(function(){
+				if(that.options.auto){
+					that.play();
+				}
+				
 			});
 		},
 		bind:function(type){
@@ -174,9 +182,11 @@
 			}
 		},
 		fade:function(){
-			
-			this.itemWrap.children(':visible').stop().fadeOut(this.options.fadeOutTime);
-			this.item.eq(this.index).stop().fadeIn(this.options.fadeInTime);
+			var that = this;
+			this.item.eq(that.index).stop(true).css({'z-index':2,'display':'block'}).animate({'opacity':1},this.options.fadeOutTime,function(){
+				that.item.eq(that.index).siblings().stop(true).css({'z-index':0,'display':'none','opacity':0});
+				that.item.eq(that.index).css({'z-index':1});
+			})
 		}
 	}
 	$.fn.zSlider = function(options){
